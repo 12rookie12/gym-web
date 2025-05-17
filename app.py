8790d2bb6a0bfc713496b957d2fa1a1f51145b2c
12rookie12 <skanas0312@gmail.com>
2025-05-17 23:02:00 +0530

    version

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os
 app = Flask(__name__)
 app.config["SESSION_TYPE"] = "filesystem" 
 app.secret_key = os.environ.get("SECRET_KEY", "supersecretkey123")  
-app.config['SESSION_COOKIE_NAME'] = 'oxy_gen_session'
+app.config["SESSION_COOKIE_NAME"]
+
 Session(app)
 
 # Serve home page
